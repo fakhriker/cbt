a3af206083180a9840b22ae496d779ae5f3cc7cb
Kerz <siraduejembutdawa@gmail.com>
2026-03-06 09:18:52 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
 const firebaseConfig = {
 
-apiKey: "APIKEY",
-authDomain: "PROJECT.firebaseapp.com",
-databaseURL: "https://PROJECT-default-rtdb.firebaseio.com",
-projectId: "PROJECT",
+apiKey: "AIzaSyAsoXOv90CkcXufuwY7SNj_qDVPk6qAS5I",
+authDomain: "ujian-cbt-1b279.firebaseapp.com",
+databaseURL: "https://ujian-cbt-1b279-default-rtdb.firebaseio.com",
+projectId: "ujian-cbt-1b279",
+storageBucket: "ujian-cbt-1b279.firebasestorage.app",
+messagingSenderId: "653458687067",
+appId: "1:653458687067:web:54319e881aaa142299eb7e"
 
-};
+}
 
 firebase.initializeApp(firebaseConfig)
 
